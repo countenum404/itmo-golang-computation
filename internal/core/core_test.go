a52aeb95b2504f78a3a414f1dde79b0a44e501e2
Denis Shabashov <countenum404@gmail.com>
2025-04-24 13:19:46 +0300

    unit test added for AsyncSolver

--- internal/core/core_test.go
+++ internal/core/core_test.go
@@ -66,3 +66,50 @@ func TestSanitySolver(t *testing.T) {
 	}
 
 }
+
+func TestSanityAsyncSolver(t *testing.T) {
+	stack := NewStack[Command]()
+	stack.Push(NewPrintCommand("c"))
+	stack.Push(NewPrintCommand("a"))
+	stack.Push(NewPrintCommand("b"))
+	stack.Push(NewPrintCommand("x"))
+
+	stack.Push(NewCalcCommand("-", "c", "a", "b"))
+	stack.Push(NewCalcCommand("/", "b", "x", "5"))
+	stack.Push(NewCalcCommand("*", "a", "x", "20"))
+	stack.Push(NewCalcCommand("+", "x", "10", "20"))
+
+	s := NewAsyncSolver(stack)
+	rs := s.Solve()
+
+	// Ожидаемые ключи и значения
+	expectedKeys := []string{"c", "a", "b", "x"}
+	expectedValues := []string{"594", "600", "6", "30"}
+
+	// Создаем мапу для хранения результатов
+	resultMap := make(map[string]string)
+
+	for rs.Size() != 0 {
+		pop, err := rs.Pop()
+		if err != nil {
+			t.Errorf("Error in stack: %s", err)
+			return
+		}
+		// Сохраняем результаты в мапу
+		actualVar := pop.Items["var"].(string)
+		actualValue := pop.Items["value"].(string)
+		resultMap[actualVar] = actualValue
+	}
+
+	// Проверяем, что все ожидаемые значения присутствуют в результатах
+	for i := 0; i < len(expectedKeys); i++ {
+		expectedKey, expectedValue := expectedKeys[i], expectedValues[i]
+		actualValue, exists := resultMap[expectedKey]
+
+		if !exists {
+			t.Errorf("Expected variable %s not found in results", expectedKey)
+		} else if actualValue != expectedValue {
+			t.Errorf("Expected variable %s to be %s; Got %s", expectedKey, expectedValue, actualValue)
+		}
+	}
+}
